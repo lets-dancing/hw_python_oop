46d9530a0856ce44ac5411b5f4ec3f9aeed7dc8d
Александр Пименов <pimenoff2010@mail.ru>
2022-01-15 16:51:20 +0300

    without recomended

--- homework.py
+++ homework.py
@@ -52,7 +52,7 @@ class Training:
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
         return InfoMessage(
-            type(self).__name__, self.duration_m,
+            self.__class__.__name__, self.duration_m,
             self.get_distance(), self.get_mean_speed(),
             self.get_spent_calories()
         )
